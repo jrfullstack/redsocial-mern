9de8d3ee1438e60257d30f0b0e6bf72dd1964c74
Jimmy Reyes <jrfullstack@gmail.com>
2023-10-02 22:25:25 +0200

    guardar follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,9 @@
+// Importar models
+const Follow = require("../models/follow");
+const User = require("../models/user");
+
+
+
 // Acciones de prueba
 const pruebaFollow = (req, res) => {
     return res.status(200).send({
@@ -5,7 +11,63 @@ const pruebaFollow = (req, res) => {
     });
 };
 
+// Accion de guardar un follow / seguir
+const save = (req, res) => { 
+    // conseguir datos por body - aqui voy a seguir
+    const params = req.body;
+
+    // sacar el usuario identificado - quien es q esta logeado
+    const identity = req.user;
+
+    // crear objeto con modelo follow
+    let userToFollow = new Follow({
+        // pasamos el usuario identificado
+        user: identity.id,
+        // userToFollow.followed = params.followed;
+        followed: params.followed,
+    });
+
+    
+    // guardar objeto en bd
+    userToFollow.save()
+                .then((followStored) => {
+        return res.status(200).send({
+            status: "success",
+            identity: req.user,
+            follow: followStored,
+        });
+    }).catch((error)=> {
+        return res.status(400).send({
+            status: "error",
+            message: "No se pudo seguir al usuario"
+        });
+    });
+    // userToFollow.save((error, followStored) => {
+
+
+    //     return res.status(200).send({
+    //         status: "success",
+    //         identity: req.user,
+    //         follow: followStored,
+    //     });
+    // })
+
+
+    
+ }
+
+// Accion de borrar un follow / dejar de seguir
+
+// Accion listado de usuarios seguido
+
+// Accion listado de usuarios que me siguen
+
+
+
+
+
 // Exportar accion
 module.exports = {
     pruebaFollow,
+    save
 };
--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const followContoller = require("../controllers/follow");
+const check = require("../middlewares/auth");
+
 
 // Definir las rutar
 router.get("/prueba-follow", followContoller.pruebaFollow);
+router.post("/save", check.auth, followContoller.save);
 
 // Exportar router
 module.exports = router;
