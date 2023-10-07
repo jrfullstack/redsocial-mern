50f89c0ddeb3f20dc578d5b9480cb1dfa399893a
Jimmy Reyes <jrfullstack@gmail.com>
2023-10-07 13:41:57 +0200

    creacion de modelo publication

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,9 +1,26 @@
+const publication = require("../models/publication");
+
+
 // Acciones de prueba
 const pruebaPublication = (req, res) => {
     return res.status(200).send({
         message: "Mensaje enviado desde: controllers/publication.js",
     });
 };
+
+// Guardar publicacion
+
+// Sacar una publicacion
+
+// Eliminar publicaciones
+
+// Listar publicaciones
+
+// Listar publicaciones de un usuario
+
+// subir ficheros
+
+// Devolver archivos multimedia - Imagenes
 
 // Exportar accion
 module.exports = {
---
+++ models/publication.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const PublicationSchema = Schema({
+    user: {
+        type: Schema.ObjectId,
+        ref: "User"
+    },
+    text: {
+        type: String,
+        require: true,
+    },
+    file: String,
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+
+Publication.Schema.plugin(mongoosePaginate);
+
+module.exports = model("Publication", PublicationSchema, "publications");
